b2267b7252c94f28c077d7dd7f5368993fb1881b
yafiabiyyu <luckyveri3@gmail.com>
2020-12-11 22:56:18 +0700

    menyelesaikan JS12

--- Assets/RaycastController.cs
+++ Assets/RaycastController.cs
@@ -8,10 +8,14 @@ public class RaycastController : MonoBehaviour
     // Start is called before the first frame update
     public float maxDistaneRay = 10f;
     public static RaycastController instance;
+    public Text birdName;
     public Transform gunFlashTarget;
     private float fireRate = 1.6f;
     private bool nextShot = true;
     private string objName = "";
+
+    AudioSource audio;
+    public AudioClip[] clips;
 
     void Awake(){
         if(instance == null) {
@@ -19,15 +23,36 @@ public class RaycastController : MonoBehaviour
         }
     }
 
+    public void playSound(int sound) {
+        audio.clip = clips[sound];
+        audio.Play();
+    }
+
     void Start()
     {
-        StartCorountine (spawnNewBird());
+        StartCoroutine (spawnNewBird());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void Fire() {
+        if(nextShot) {
+            StartCoroutine (takeShot());
+        }
+    }
+
+    private IEnumerator takeShot(){
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f,0.5f,0));
+        RaycastHit hit;
+        GameObject gunFlash = Instantiate(Resources.Load("gunFlashSmoke", typeof(GameObject))) as GameObject;
+        gunFlash.transform.position = gunFlashTarget.position;
+        yield return new WaitForSeconds(fireRate);
+        nextShot = true;
+
     }
 
     private IEnumerator spawnNewBird(){
